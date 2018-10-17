ed0a50656fb9df70b02b659735096316fce4077d
Adam Eldridge <adam@thinlamp.co.uk>
2018-10-18 00:24:45 +0100

    convert id to binary

--- convert_id/convert_id.py
+++ convert_id/convert_id.py
@@ -1,16 +1,20 @@
-# TODO: take input id as a parameter from command line
-input_id = '001A000010khO8J'
+import re
 
-# Split the string into groups of 5 characters
+# TODO: take input id as a parameter from command line
+input_id = '001A000010khP8J'
+
+# Split id into list of 5 character chunks
 split_id = [input_id[i:i+5] for i in range(0, len(input_id), 5)]
 print 'split_id: ' + str(split_id)
 
 # Reverse the strings in split_id
-split_id_reversed = [group[::-1] for group in split_id]
+split_id_reversed = [chunk[::-1] for chunk in split_id]
 print 'split_id_reversed: ' + str(split_id_reversed)
 
 # TODO: Convert uppercase chars to 1 and lowercase to 0
-
+split_id_binary = [re.sub(r'[a-z,2-9]', "0", chunk) for chunk in split_id_reversed]
+split_id_binary = [re.sub(r'[A-Z]', "1", chunk) for chunk in split_id_reversed]
+print 'split_id_binary: ' + str(split_id_binary)
 
 # TODO: Look up the corresponding char in the BinaryIdLookup
 
